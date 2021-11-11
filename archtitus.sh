0b6280f53e3c706f8757c30fcc55a8072a094478
jgrf77 <justinfletcher90@gmail.com>
2021-11-11 20:40:09 +1300

    added test comment

--- archtitus.sh
+++ archtitus.sh
@@ -13,3 +13,5 @@ Test is now complete
 #    source /mnt/root/ArchTitus/install.conf
 #    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
 #    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+
+#This is a test comment
