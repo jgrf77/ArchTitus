834b0361035bba6bbac4ce28830f1b002b54c1b0
jgrf77 <justinfletcher90@gmail.com>
2021-11-12 20:36:39 +1300

    trial parted

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -3,9 +3,40 @@
 ##################################################
 #		    Variables 			 #
 ##################################################
+##################################################
 
 #Set variable SCRIPT_DIR
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+##################################################
+########## Hard Disk Partitioning Variable
+# ANTENTION, this script erases ALL YOU HD DATA (specified bt $HD)
+HD=/dev/sda
+# Boot Partition Size: /boot
+BOOT_SIZE=200
+# Root Partition Size: /
+ROOT_SIZE=10000
+# Swap partition size: /swap
+SWAP_SIZE=2000
+# The /home partition will occupy the remain free space
+
+# Partitions file system
+BOOT_FS=ext4
+HOME_FS=ext4
+ROOT_FS=ext4
+
+######## Auxiliary variables. THIS SHOULD NOT BE ALTERED
+BOOT_START=1
+BOOT_END=$(($BOOT_START+$BOOT_SIZE))
+
+SWAP_START=$BOOT_END
+SWAP_END=$(($SWAP_START+$SWAP_SIZE))
+
+ROOT_START=$SWAP_END
+ROOT_END=$(($ROOT_START+$ROOT_SIZE))
+
+HOME_START=$ROOT_END
+#####################################################
 
 
 
@@ -53,7 +84,63 @@ read -p "are you sure you want to continue (Y/N):" formatdisk
 #If $formatdisk is yes then run disc format commands
 case $formatdisk in
 y|Y|yes|Yes|YES)
-cfdisk
+#cfdisk
+
+############################################################################
+############################################################################
+#### Partitioning
+echo "HD Initialization"
+# Set the partition table to MS-DOS type 
+parted -s $HD mklabel msdos &> /dev/null
+
+# Remove any older partitions
+parted -s $HD rm 1 &> /dev/null
+parted -s $HD rm 2 &> /dev/null
+parted -s $HD rm 3 &> /dev/null
+parted -s $HD rm 4 &> /dev/null
+
+# Create boot partition
+echo "Create boot partition"
+parted -s $HD mkpart primary $BOOT_FS $BOOT_START $BOOT_END 1>/dev/null
+parted -s $HD set 1 boot on 1>/dev/null
+
+# Create swap partition
+echo "Create swap partition"
+parted -s $HD mkpart primary linux-swap $SWAP_START $SWAP_END 1>/dev/null
+
+# Create root partition
+echo "Create root partition"
+parted -s $HD mkpart primary $ROOT_FS $ROOT_START $ROOT_END 1>/dev/null
+
+# Create home partition
+echo "Create home partition"
+parted -s -- $HD mkpart primary $HOME_FS $HOME_START -0 1>/dev/null
+
+# Formats the root, home and boot partition to the specified file system
+echo "Formating boot partition"
+mkfs.$BOOT_FS /dev/sda1 -L Boot 1>/dev/null
+echo "Formating root partition"
+mkfs.$ROOT_FS /dev/sda3 -L Root 1>/dev/null
+echo "Formating home partition"
+mkfs.$HOME_FS /dev/sda4 -L Home 1>/dev/null
+# Initializes the swap
+echo "Formating swap partition"
+mkswap /dev/sda2
+swapon /dev/sda2
+
+
+echo "Mounting partitions"
+# mounts the root partition
+mount /dev/sda3 /mnt
+# mounts the boot partition
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+# mounts the home partition
+mkdir /mnt/home
+mount /dev/sda4 /mnt/home
+###################################################
+###################################################
+
 ;;
 *) echo "you have declined disc formatting"
 echo "Your partition table remains:"
@@ -66,21 +153,21 @@ echo "Your partition table is now:
 "
 fdisk -l
 
-#Format the root partition: 
-echo "Formatting root partition"
-mkfs.ext4 /dev/${DISK}1 #needs to be made generic
+##Format the root partition: 
+#echo "Formatting root partition"
+#mkfs.ext4 /dev/${DISK}1 #needs to be made generic
 
-#Initialise the swap partition: 
-echo "Initialising swap partition"
-mkswap /dev/${DISK}2 #needs to be made generic
+##Initialise the swap partition: 
+#echo "Initialising swap partition"
+#mkswap /dev/${DISK}2 #needs to be made generic
 
-#Mount the filesystem: 
-echo "Mounting filesystem"
-mount /dev/${DISK}1 /mnt #needs to be made generic
+##Mount the filesystem: 
+#echo "Mounting filesystem"
+#mount /dev/${DISK}1 /mnt #needs to be made generic
 
-#Enable swap: 
-echo "Enabling swap"
-swapon /dev/${DISK}2 #needs to be made generic
+##Enable swap: 
+#echo "Enabling swap"
+#swapon /dev/${DISK}2 #needs to be made generic
 
 echo "-------------------------------------------------"
 echo "Installing base linux linux-firmware nano"
