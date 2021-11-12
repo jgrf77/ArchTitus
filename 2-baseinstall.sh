12be4b4d623a89437fed4874c0353115d209fca5
jgrf77 <justinfletcher90@gmail.com>
2021-11-12 23:15:24 +1300

    reposition /home/justin/git/ArchTitus variable

--- 2-baseinstall.sh
+++ 2-baseinstall.sh
@@ -10,6 +10,9 @@ pacstrap /mnt base linux linux-firmware nano
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
+#Set variable SCRIPT_DIR
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 #Copy install script to new Arch install
 cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
 
